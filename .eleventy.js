a758cc5169f7f0521055a7d7526e7cf804e01676
madjalapeno <github@jalapeno.co.uk>
2025-02-21 20:12:59 -0500

    feat: add NormaTec portfolio entry and enhance layout structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const now = String(Date.now())
+const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 
 module.exports = function (eleventyConfig) {
 
@@ -11,6 +12,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("src");
 
   eleventyConfig.addGlobalData("siteName", "jalapeno.design");
+
+  // Plug Ins
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 
   eleventyConfig.addShortcode('version', function () {
     return now
--- src/portfolio/normatec.md
+++
@@ -1,8 +0,0 @@
----
-title: NormaTec
-layout: portfolio.njk
-description: leg pumpy thing
-author: jalapeno.design
-keywords: dfm, system, assembly
----
-pumpy thing
